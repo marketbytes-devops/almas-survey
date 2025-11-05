8513a302378c229c7f51df563e917176eb6d6c10
ajay-marketbytes <ajay@marketbytes.in>
2025-11-05 21:55:59 +0530

    Updates

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
  
 const apiClient = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "http://almassurvey.marketbytes.in/api",
   headers: {
     "Content-Type": "application/json",
   },
@@ -10,7 +10,7 @@ const apiClient = axios.create({
 const getAbsoluteImageUrl = (url) => {
   if (!url) return null;
   if (url.startsWith("http")) return url;
-  return `http://127.0.0.1:8000${url}`;
+  return `http://almassurvey.marketbytes.in${url}`;
 };
  
 apiClient.interceptors.request.use(
