00889c138d654b700ff41feda542b699c8fb5047
marketbytes-devops <marketbytesdevops@gmail.com>
2026-01-07 13:12:45 +0530

    updates bookings fields as null ture blank true

--- backend/bookings/models.py
+++ backend/bookings/models.py
@@ -26,8 +26,8 @@ class Booking(models.Model):
         ]
     )
     
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
 
     def save(self, *args, **kwargs):
         if not self.booking_id:
